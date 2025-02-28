85e904ec07c9b8b62d1ed0519aa5314d693983fc
wonseokyoon <01088115030a@gmail.com>
2025-02-28 22:05:04 +0900

    feat:실시간 메세지 전송

--- backend/src/main/java/com/NBE_4_5_2/Team5/domain/chatting/service/ChatService.java
+++ backend/src/main/java/com/NBE_4_5_2/Team5/domain/chatting/service/ChatService.java
@@ -1,6 +1,8 @@
 package com.NBE_4_5_2.Team5.domain.chatting.service;
 
+import com.NBE_4_5_2.Team5.domain.chatting.entity.ChatMessage;
 import com.NBE_4_5_2.Team5.domain.chatting.entity.ChatRoom;
+import com.NBE_4_5_2.Team5.domain.chatting.repository.ChatMessageRepository;
 import com.NBE_4_5_2.Team5.domain.chatting.repository.ChatRoomRespository;
 import com.NBE_4_5_2.Team5.domain.user.entity.Users;
 import com.NBE_4_5_2.Team5.domain.user.repository.UserRepository;
@@ -8,6 +10,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -16,6 +19,8 @@ import java.util.Optional;
 public class ChatService {
     private final ChatRoomRespository chatRoomRespository;
     private final UserRepository userRepository;
+    private final ChatMessageRepository chatMessageRepository;
+
 
     public ChatRoom createRoom(Users sender, Users receiver) {
 
@@ -26,6 +31,13 @@ public class ChatService {
         return chatRoomRespository.save(chatRoom);
     }
 
+    public ChatMessage saveMessage(ChatMessage message) {
+        return chatMessageRepository.save(message); // 메시지 저장
+    }
+
+    public List<ChatMessage> getMessagesByRoomId(String roomId) {
+        return chatMessageRepository.findByChatRoomId(roomId);
+    }
 
     public ChatRoom findById(String roomId) {
         Optional<ChatRoom> chatRoom=chatRoomRespository.findById(roomId);
--- backend/src/main/java/com/NBE_4_5_2/Team5/global/config/WebSocketConfig.java
+++ backend/src/main/java/com/NBE_4_5_2/Team5/global/config/WebSocketConfig.java
@@ -1,34 +1,21 @@
 package com.NBE_4_5_2.Team5.global.config;
 
+import com.NBE_4_5_2.Team5.global.handler.ChatHandler;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.messaging.simp.config.MessageBrokerRegistry;
-import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
-import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
-import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
 
 @Configuration
-@EnableWebSocketMessageBroker
-public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+@EnableWebSocket
+public class WebSocketConfig implements WebSocketConfigurer {
 
     @Override
-    public void configureMessageBroker(MessageBrokerRegistry config) {
-        // 메세지 구독 경로
-        config.enableSimpleBroker("/topic");
-        // 메세지 발행 경로 설정
-        config.setApplicationDestinationPrefixes("/app");
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+        //
+        registry.addHandler(new ChatHandler(), "/ws/chat")  // 소켓 연결 주소
+                // CORS 허용
+                .setAllowedOrigins("*");
+
     }
-
-    @Override
-    public void registerStompEndpoints(StompEndpointRegistry registry) {
-        // 채팅
-        registry.addEndpoint("/chat")
-                .setAllowedOrigins("*")
-                .withSockJS();
-        // 매칭
-        registry.addEndpoint("/match")
-                .setAllowedOrigins("*")
-                .withSockJS();
-    }
-
-
 }
