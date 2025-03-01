fc467b9a9fecea0a93dbdc4f8d47f11c2ee211eb
omegafrog <jiwoo22110103@gmail.com>
2025-03-01 12:54:39 +0900

    feat: 페이로 결제 서비스 작성
    로그인한 유저와 구매할 상품 엔티티를 조회
    유저가 이 상품을 살 수 있는지 체크
    이후 구매 로직 진행
    
    새로운 상품에 대해서 Payment 객체 생성.
    이때 totalAmount는 -amount 로 설정함.
    totalAmount가 양수면 페이 충전, 음수면 페이 사용
    
    이후 새로 생성한 Payment객체의 Dto를 리턴함.

--- backend/src/main/java/com/NBE_4_5_2/Team5/domain/payment/service/PaymentService.java
+++ backend/src/main/java/com/NBE_4_5_2/Team5/domain/payment/service/PaymentService.java
@@ -1,6 +1,7 @@
 package com.NBE_4_5_2.Team5.domain.payment.service;
 
 import java.util.Map;
+import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
@@ -11,11 +12,15 @@ import org.springframework.web.client.RestClient;
 
 import com.NBE_4_5_2.Team5.domain.member.entity.Member;
 import com.NBE_4_5_2.Team5.domain.member.repository.MemberRepository;
+import com.NBE_4_5_2.Team5.domain.payment.dto.PaymentDto;
 import com.NBE_4_5_2.Team5.domain.payment.dto.PaymentMetaData;
 import com.NBE_4_5_2.Team5.domain.payment.entity.Payment;
 import com.NBE_4_5_2.Team5.domain.payment.enums.PaymentStatus;
 import com.NBE_4_5_2.Team5.domain.payment.repository.PaymentRepository;
+import com.NBE_4_5_2.Team5.domain.product.entity.Product;
+import com.NBE_4_5_2.Team5.domain.product.repository.ProductRepository;
 import com.NBE_4_5_2.Team5.global.exception.TossPaymentException;
+import com.NBE_4_5_2.Team5.global.exception.product.ProductNotFoundException;
 
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
@@ -32,6 +37,7 @@ public class PaymentService {
 
 	private final PaymentRepository paymentRepository;
 	private final MemberRepository memberRepository;
+	private final ProductRepository productRepository;
 
 	@Value("${custom.toss.payment.secret}")
 	private String tossPaymentSecretKey;
@@ -49,6 +55,28 @@ public class PaymentService {
 
 	private Member getUser() {
 		return memberRepository.findAll().get(0);
+	}
+
+	public PaymentDto purchase(String productId, Integer amount) {
+
+		Member loggedInUser = getUser();
+		Product product = productRepository.findById(productId)
+			.orElseThrow(() -> new ProductNotFoundException("id가 %s인 Payment를 찾을 수 없습니다.".formatted(productId)));
+
+		// 할인 등 product 가격과 총 결제 금액이 다를 수 있으므로 amount를 따로 받음.
+		loggedInUser.canBuy(product, amount);
+		loggedInUser.buy(product, amount);
+
+		Payment purchasedPayment = Payment.builder()
+			.id("payment-" + UUID.randomUUID())
+			.buyer(loggedInUser)
+			.totalPrice(-1 * amount)
+			.status(PaymentStatus.DONE)
+			.build();
+
+		Payment saved = paymentRepository.save(purchasedPayment);
+
+		return PaymentDto.of(saved);
 	}
 
 	@Getter
