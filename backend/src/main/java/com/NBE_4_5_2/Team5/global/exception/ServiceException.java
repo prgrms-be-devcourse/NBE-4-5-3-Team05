b2b39af85b7f8ed0c5f9ad349d72fc482808fbfd
omegafrog <jiwoo22110103@gmail.com>
2025-03-02 14:03:03 +0900

    feat: 공유한 global 추가

---
+++ backend/src/main/java/com/NBE_4_5_2/Team5/global/exception/ServiceException.java
@@ -0,0 +1,24 @@
+package com.NBE_4_5_2.Team5.global.exception;
+
+import com.NBE_4_5_2.Team5.global.dto.RsData;
+
+public class ServiceException extends RuntimeException {
+	private final RsData<?> rsData;
+
+	public ServiceException(String code, String message) {
+		super(message);
+		rsData = new RsData<>(code, message);
+	}
+
+	public String getCode() {
+		return rsData.getCode();
+	}
+
+	public String getMsg() {
+		return rsData.getMessage();
+	}
+
+	public int getStatusCode() {
+		return rsData.getStatusCode();
+	}
+}
