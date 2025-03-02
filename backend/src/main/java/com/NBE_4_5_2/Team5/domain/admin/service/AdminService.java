ca9e8205cfcb633fa862d681e1b770f009f09724
omegafrog <jiwoo22110103@gmail.com>
2025-03-02 14:25:11 +0900

    feat: adminService 추가

---
+++ backend/src/main/java/com/NBE_4_5_2/Team5/domain/admin/service/AdminService.java
@@ -0,0 +1,48 @@
+package com.NBE_4_5_2.Team5.domain.admin.service;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.NBE_4_5_2.Team5.domain.admin.dto.NoticeDto;
+import com.NBE_4_5_2.Team5.domain.admin.entity.Notice;
+import com.NBE_4_5_2.Team5.domain.admin.repository.NoticePostRepository;
+import com.NBE_4_5_2.Team5.domain.user.entity.Role;
+import com.NBE_4_5_2.Team5.domain.user.entity.User;
+import com.NBE_4_5_2.Team5.domain.user.repository.UserRepository;
+import com.NBE_4_5_2.Team5.global.exception.ServiceException;
+
+import jakarta.validation.constraints.NotEmpty;
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
+public class AdminService {
+	private final UserRepository userRepository;
+	private final NoticePostRepository noticePostRepository;
+
+	public NoticeDto writeNotice(@NotEmpty String title, @NotEmpty String content) {
+		User admin = getUser();
+
+		isAdmin(admin);
+
+		Notice notice = Notice.builder().title(title).content(content)
+			.admin(admin).build();
+
+		Notice saved = noticePostRepository.save(notice);
+
+		return NoticeDto.of(saved);
+
+	}
+
+	private void isAdmin(User admin) {
+		if (!admin.getRole().equals(Role.ADMIN)) {
+			throw new ServiceException(HttpStatus.BAD_REQUEST.toString(), "관리자만 작성할 수 있는 글입니다.");
+		}
+	}
+
+	private User getUser() {
+		return userRepository.findAllByRole(Role.ADMIN).get(0);
+	}
+}
