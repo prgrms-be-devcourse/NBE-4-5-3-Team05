37bfde3ae39a588bc16865ac29dcf487fd18c203
wonseokyoon <01088115030a@gmail.com>
2025-03-02 15:46:59 +0900

    feat: 채팅방 생성,메세지 전송

--- backend/src/main/java/com/NBE_4_5_2/Team5/domain/chatting/dto/ChatRoomRequest.java
+++
@@ -1,9 +0,0 @@
-package com.NBE_4_5_2.Team5.domain.chatting.dto;
-
-import lombok.Data;
-
-@Data
-public class ChatRoomRequest {
-    private String senderName;
-    private String receiverName;
-}
--- backend/src/main/java/com/NBE_4_5_2/Team5/domain/chatting/repository/ChatRoomRespository.java
+++
@@ -1,9 +0,0 @@
-package com.NBE_4_5_2.Team5.domain.chatting.repository;
-
-import com.NBE_4_5_2.Team5.domain.chatting.entity.ChatRoom;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-
-public interface ChatRoomRespository extends JpaRepository<ChatRoom, String> {
-
-}
