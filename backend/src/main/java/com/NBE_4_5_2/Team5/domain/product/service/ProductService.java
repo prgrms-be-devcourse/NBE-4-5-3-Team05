db4835dda9179cf639664485e91db5945893057f
sojunsik <chunsik1003@gmail.com>
2025-03-03 20:56:13 +0900

    feat service 추가

--- backend/src/main/java/com/NBE_4_5_2/Team5/domain/product/service/ProductService.java
+++ backend/src/main/java/com/NBE_4_5_2/Team5/domain/product/service/ProductService.java
@@ -2,6 +2,7 @@ package com.NBE_4_5_2.Team5.domain.product.service;
 
 import com.NBE_4_5_2.Team5.domain.product.dto.ProductDto;
 import com.NBE_4_5_2.Team5.domain.product.entity.Product;
+import com.NBE_4_5_2.Team5.domain.product.repository.LikedPostRepository;
 import com.NBE_4_5_2.Team5.domain.product.repository.ProductRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -12,6 +13,7 @@ import java.util.stream.Collectors;
 @Service@RequiredArgsConstructor
 public class ProductService {
     private final ProductRepository productRepository;
+    private final LikedPostRepository likedPostRepository;
 
 
     //구매 내역 조회
@@ -26,6 +28,15 @@ public class ProductService {
     public List<ProductDto> getSalesHistory(String userId, String status) {
         List<Product> products = productRepository.findAllBySellerIdAndStatus(userId, status);
         return products.stream()
+                .map(ProductDto::fromEntity)
+                .collect(Collectors.toList());
+    }
+
+    // 찜한 게시글 조회
+    public List<ProductDto> getFavoriteProducts(String userId) {
+        List<String> likedProductIds = likedPostRepository.findAllProductPostIdsByUserId(userId);
+        List<Product> favoriteProducts = productRepository.findAllById(likedProductIds);
+        return favoriteProducts.stream()
                 .map(ProductDto::fromEntity)
                 .collect(Collectors.toList());
     }
