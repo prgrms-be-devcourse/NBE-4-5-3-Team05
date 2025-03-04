0a39102ce3658dc42603837510b9c9ae0f227f76
omegafrog <jiwoo22110103@gmail.com>
2025-03-04 16:39:41 +0900

    refactor: 패키지 구조 맞춤

--- backend/src/main/java/com/NBE_4_5_2/Team5/domain/member/repository/MemberRepository.java
+++
@@ -1,8 +0,0 @@
-package com.NBE_4_5_2.Team5.domain.member.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.NBE_4_5_2.Team5.domain.member.entity.Member;
-
-public interface MemberRepository extends JpaRepository<Member, String> {
-}
