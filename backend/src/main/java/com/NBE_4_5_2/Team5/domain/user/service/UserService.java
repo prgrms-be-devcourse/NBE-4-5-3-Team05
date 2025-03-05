0104217233a08fdac6da6469098d86f51712eeee
omegafrog <jiwoo22110103@gmail.com>
2025-03-05 11:12:44 +0900

    feat: accessToken에 Role 추가

--- backend/src/main/java/com/NBE_4_5_2/Team5/domain/user/service/UserService.java
+++ backend/src/main/java/com/NBE_4_5_2/Team5/domain/user/service/UserService.java
@@ -95,4 +95,25 @@ public class UserService {
         return userRepository.count();
     }
 
+	public User getUserIdentity() {
+		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+		if (authentication == null) {
+			throw new ServiceException("401-2", "로그인이 필요합니다.");
+		}
+
+		Object principal = authentication.getPrincipal();
+
+		if (!(principal instanceof SecurityUser)) {
+			throw new ServiceException("401-3", "잘못된 인증 정보입니다");
+		}
+
+		SecurityUser user = (SecurityUser)principal;
+
+		return User.builder()
+			.id(user.getId())
+			.username(user.getUsername())
+			.role(user.getRole())
+			.build();
+	}
 }
--- backend/src/main/java/com/NBE_4_5_2/Team5/global/security/SecurityUser.java
+++ backend/src/main/java/com/NBE_4_5_2/Team5/global/security/SecurityUser.java
@@ -1,18 +1,25 @@
 package com.NBE_4_5_2.Team5.global.security;
 
-import lombok.Getter;
+import java.util.Collection;
+
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 
-import java.util.Collection;
+import com.NBE_4_5_2.Team5.domain.user.entity.Role;
+
+import lombok.Getter;
 
 public class SecurityUser extends User {
 
-    @Getter
-    private String id;
+	@Getter
+	private String id;
+	@Getter
+	private Role role;
 
-    public SecurityUser(String id, String username, String password, Collection<? extends GrantedAuthority> authorities) {
-        super(username, password, authorities);
-        this.id = id;
-    }
+	public SecurityUser(String id, String username, String password,
+		Collection<? extends GrantedAuthority> authorities, Role role) {
+		super(username, password, authorities);
+		this.id = id;
+		this.role = role;
+	}
 }
