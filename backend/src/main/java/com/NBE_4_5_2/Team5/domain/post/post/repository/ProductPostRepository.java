9dc585f728d676dd294211cd9977358fe2ab2088
sojunsik <chunsik1003@gmail.com>
2025-03-05 10:31:51 +0900

    refactor repository 추가

--- backend/src/main/java/com/NBE_4_5_2/Team5/domain/post/post/repository/ProductPostRepository.java
+++ backend/src/main/java/com/NBE_4_5_2/Team5/domain/post/post/repository/ProductPostRepository.java
@@ -1,6 +1,7 @@
 package com.NBE_4_5_2.Team5.domain.post.post.repository;
 
 import com.NBE_4_5_2.Team5.domain.post.post.entity.ProductPost;
+import com.NBE_4_5_2.Team5.domain.post.post.enums.ProductStatus;
 import com.NBE_4_5_2.Team5.domain.user.entity.User;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -9,6 +10,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface ProductPostRepository extends JpaRepository<ProductPost, String> {
@@ -23,4 +26,12 @@ public interface ProductPostRepository extends JpaRepository<ProductPost, String
 
     @EntityGraph(attributePaths = {"productCategories.category"})
     Page<ProductPost> findByWriter(User writer, Pageable pageable);
+
+    // 페이징 없이 전체 조회
+    @EntityGraph(attributePaths = {"productCategories.category"})
+    List<ProductPost> findByWriter(User writer);
+
+    //구매된(판매 완료) 상품들 조회
+    @EntityGraph(attributePaths = {"productCategories.category"})
+    List<ProductPost> findAllByStatus(ProductStatus status);
 }
