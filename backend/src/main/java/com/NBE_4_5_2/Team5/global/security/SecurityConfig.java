dae42f152309762e8ae4d79eb0bc516e3e7b38de
asungkim <130833844+asungkim@users.noreply.github.com>
2025-03-07 08:45:45 +0900

    [fix] 누락 된 부분 추가 (#63)
    * SecurityConfig 누락된 oauth 추가
    * CorsConfig와 CorsFilter는 겹치는 부분이라 일단 CorsConfig를 SecurityConfig에서 제외했음 - 추후 삭제 가능

--- backend/src/main/java/com/NBE_4_5_2/Team5/global/security/SecurityConfig.java
+++ backend/src/main/java/com/NBE_4_5_2/Team5/global/security/SecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.header.writers.frameoptions.XFrameOptionsHeaderWriter;
@@ -16,7 +17,10 @@ import org.springframework.security.web.header.writers.frameoptions.XFrameOption
 @RequiredArgsConstructor
 public class SecurityConfig {
 
+    private final CorsConfig corsConfig;
     private final CustomAuthenticationFilter customAuthenticationFilter;
+    private final CustomAuthorizationRequestResolver customAuthorizationRequestResolver;
+    private final CustomAuthenticationSuccessHandler customAuthenticationSuccessHandler;
 
     @Bean
     SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -29,6 +33,16 @@ public class SecurityConfig {
                         .addHeaderWriter(new XFrameOptionsHeaderWriter(
                                 XFrameOptionsHeaderWriter.XFrameOptionsMode.SAMEORIGIN)))
                 .csrf(csrf -> csrf.disable())
+                .sessionManagement(session -> {
+                    session.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+                })
+//                .cors(cors -> cors.configurationSource(corsConfig))
+                .oauth2Login((oauth2) -> {
+                    oauth2.authorizationEndpoint(
+                            authorizationEndpoint -> authorizationEndpoint.authorizationRequestResolver(customAuthorizationRequestResolver)
+                    );
+                    oauth2.successHandler(customAuthenticationSuccessHandler);
+                })
                 .addFilterBefore(customAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
                 .exceptionHandling(
                         exceptionHandling -> exceptionHandling
