30f05a4ffcdec5c2a8ec80adde866f00275c6459
sojunsik <chunsik1003@gmail.com>
2025-03-07 10:44:52 +0900

    feat api.json 파일 생성 및 frontend 폴더에 schema.d.ts 파일 생성

---
+++ backend/src/main/java/com/NBE_4_5_2/Team5/global/springDocs/SpringDocConfig.java
@@ -0,0 +1,23 @@
+package com.NBE_4_5_2.Team5.global.springDocs;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+@OpenAPIDefinition(info = @Info(title = "API 서버", version = "v1"))
+public class SpringDocConfig {
+
+    @Bean
+    public GroupedOpenApi groupApiV1() {
+        return GroupedOpenApi.builder()
+                .group("api")
+                .pathsToMatch("/api/**")
+                .build();
+    }
+
+
+}
