bd1534b8a0a360002895391f897807617f21db50
omegafrog <jiwoo22110103@gmail.com>
2025-03-07 19:12:27 +0900

    chore: update build file

--- backend/scripts/start_app.sh
+++ backend/scripts/start_app.sh
@@ -6,9 +6,10 @@ cd /home/ec2-user/app
 # Example: pass secrets as environment variables
 # (You can store these in CodeDeploy environment configuration,
 #  or use parameter store or secrets manager)
-export JWT_SECRET="$JWT_SECRET"# This references the env var set by CodeDeploy
-export TOSS_SECRET="$TOSS_SECRET"
-export KAKAO_CLIENT_ID="$KAKAO_CLIENT_ID"
+# This references the env var set by CodeDeploy
+export JWT_SECRET=$(aws ssm get-parameter --name /team5/JWT_SECRET --with-decryption --query "Parameter.Value" --output text)
+export TOSS_SECRET=$(aws ssm get-parameter --name /team5/TOSS_SECRET --with-decryption --query "Parameter.Value" --output text)
+export KAKAO_CLIENT_ID=$(aws ssm get-parameter --name /team5/KAKAO_CLIENT_ID --with-decryption --query "Parameter.Value" --output text)
 
 # Start your .jar in background (example)
-nohup java -jar myapp.jar > /home/ec2-user/app/app.log 2>&1 &
+nohup java -jar /home/ubuntu/app/build/libs/*.jar > /home/ubuntu/app/app.log 2>&1 &
