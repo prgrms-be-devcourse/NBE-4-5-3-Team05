de9cc49b63fc082fd597fcbcc0ec4ed2a8a3d423
omegafrog <jiwoo22110103@gmail.com>
2025-03-08 10:08:04 +0900

    feat: db를 rds로 변경

--- backend/scripts/start_app.sh
+++ backend/scripts/start_app.sh
@@ -34,6 +34,7 @@ fi
 export JWT_SECRET=$(aws ssm get-parameter --name /team5/JWT_SECRET --with-decryption --query "Parameter.Value" --output text)
 export TOSS_SECRET=$(aws ssm get-parameter --name /team5/TOSS_SECRET --with-decryption --query "Parameter.Value" --output text)
 export KAKAO_CLIENT_ID=$(aws ssm get-parameter --name /team5/KAKAO_CLIENT_ID --with-decryption --query "Parameter.Value" --output text)
+export DB_PASSWORD=$(aws ssm get-parameter --name /team5/DB_PASSWORD --with-decryption --query "Parameter.Value" --output text)
 
 # Start your .jar in background (example)
-nohup java -jar /home/ubuntu/app/build/libs/*.jar > /home/ubuntu/app/app.log 2>&1 &
+nohup java -jar /home/ubuntu/app/build/libs/*.jar --spring.profiles.active=dev,secret > /home/ubuntu/app/app.log 2>&1 &
