a768750f8ff0a23dc5400ef2f3ad1f66cf6d3878
jeonghyeon-ohk <ok6737@naver.com>
2025-03-09 21:00:14 +0900

    fix: 로그아웃 시 Redis의 보조 인덱스로 설정된 refreshToken의 정보가 제거되지 않는 문제 발생
    - key로 설정한 userId 값만 제거되고 refreshToken에 대한 index 정보는 유지되는 문제
    - RedisConfig를 생성하고 enableKeyspaceEvents를 어노테이션으로 설정하여 해결

---
+++ backend/src/main/java/com/NBE_4_5_2/Team5/global/config/RedisConfig.java
@@ -0,0 +1,26 @@
+package com.NBE_4_5_2.Team5.global.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisKeyValueAdapter;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
+
+@Configuration
+@EnableRedisRepositories(enableKeyspaceEvents = RedisKeyValueAdapter.EnableKeyspaceEvents.ON_STARTUP)
+public class RedisConfig {
+
+    @Bean
+    LettuceConnectionFactory connectionFactory() {
+        return new LettuceConnectionFactory();
+    }
+
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate() {
+        RedisTemplate<String, Object> template = new RedisTemplate<>();
+        template.setConnectionFactory(connectionFactory());
+        return template;
+    }
+
+}
