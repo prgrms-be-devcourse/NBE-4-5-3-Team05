b6e007b5626bb341dc06558967e3cadc42283f60
omegafrog <jiwoo22110103@gmail.com>
2025-03-13 16:37:52 +0900

    feat: product list 아코디언 추가

--- frontend/src/app/admin/_pages/ProductListAdmin.tsx
+++ frontend/src/app/admin/_pages/ProductListAdmin.tsx
@@ -1,5 +1,62 @@
+"use client";
+
+import { components } from "@/lib/backend/apiV1/schema";
+import client from "@/lib/client";
+import { useEffect, useState } from "react";
+import ProductListAccordian from "./ProductListAccordian";
+import PageSizeSelector from "./PageSizeSelector";
+import Pagination from "./Pagination";
+
+type ProductListItem = components["schemas"]["PreviewPostResponse"];
+
 export default function ProductListAdmin() {
-  return <div>
-    <h1>ProductList</h1>
-  </div>;
+  const [page, setPage] = useState<number>(1);
+  const [pageSize, setPageSize] = useState<number>(10);
+  const [keyword, setKeyword] = useState<string>("");
+  const [sort, setSort] = useState<string>("desc");
+  const [productList, setProductList] = useState<ProductListItem[]>([]);
+  const [totalPages, setTotalPages] = useState<number>(0);
+  useEffect(() => {
+    getProductList();
+  }, [page, pageSize, keyword, sort]);
+  const getProductList = async () => {
+    const result = await client.GET("/api/posts", {
+      params: {
+        query: {
+          keyword,
+          page,
+          pageSize,
+          sort,
+        },
+      },
+    });
+    if (result.error) {
+      console.log(result.response);
+    }
+    setProductList(result.data!.data.items);
+    setTotalPages(result.data!.data.totalPages);
+  };
+
+  return (
+    <div className="w-full">
+      <div className="flex-1 flex flex-col ">
+        <h1>ProductList</h1>
+        <div className="flex justify-end items-end">
+          <PageSizeSelector
+            selectedPageSize={pageSize}
+            setSelectedPageSize={setPageSize}
+          ></PageSizeSelector>
+        </div>
+
+        <ProductListAccordian items={productList}></ProductListAccordian>
+        <Pagination
+          currentPage={page}
+          onPageChange={(page) => {
+            setPage(page);
+          }}
+          totalPages={totalPages}
+        ></Pagination>
+      </div>
+    </div>
+  );
 }
