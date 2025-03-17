489104a322e6cdf2813d4a87aed09bc8d630d65f
omegafrog <jiwoo22110103@gmail.com>
2025-03-17 19:47:32 +0900

    feat: 댓글 삭제 구현

--- frontend/src/app/posts/[postId]/_pages/comments.tsx
+++ frontend/src/app/posts/[postId]/_pages/comments.tsx
@@ -49,6 +49,26 @@ export default function Comments({
     setContent("");
   };
 
+  const deleteComment = async (comment: commentDto) => {
+    const result = await client.DELETE(
+      "/api/posts/{post-id}/comments/{comment-id}",
+      {
+        params: {
+          path: {
+            "comment-id": comment.id!,
+            "post-id": postId!,
+          },
+        },
+        credentials: "include",
+      }
+    );
+    if (result.error) {
+      console.log(result.error);
+      return;
+    }
+    setComments((prev) => prev.filter((item) => item.id !== comment.id));
+  };
+
   useEffect(() => {
     console.log("Initial Comments Updated:", initialComments);
     setComments(initialComments);
@@ -71,7 +91,7 @@ export default function Comments({
             sendComment();
           }}
         >
-          "작성하기"
+          작성하기
         </Button>{" "}
       </div>
       <div className="space-y-2">
@@ -80,6 +100,14 @@ export default function Comments({
           return (
             <div key={comment.id} className="p-2 border-b">
               {comment.content}
+              <Button
+                onClick={(e) => {
+                  e.preventDefault();
+                  deleteComment(comment);
+                }}
+              >
+                삭제하기
+              </Button>
             </div>
           );
         })}
