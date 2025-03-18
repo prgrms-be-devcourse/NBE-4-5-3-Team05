40ca7551f4437f772bb562003d9a5aa7dbac2e72
omegafrog <jiwoo22110103@gmail.com>
2025-03-18 10:13:45 +0900

    chore: prod 쿠키 도메인 설정

--- backend/src/main/java/com/NBE_4_5_2/Team5/global/ProductionRq.java
+++ backend/src/main/java/com/NBE_4_5_2/Team5/global/ProductionRq.java
@@ -2,6 +2,7 @@ package com.NBE_4_5_2.Team5.global;
 
 import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 import org.springframework.web.context.annotation.RequestScope;
@@ -22,6 +23,9 @@ public class ProductionRq implements Rq {
 	private final HttpServletResponse response;
 	private final HttpServletRequest request;
 	private final UserService userService;
+
+	@Value("${custom.domain}")
+	private String domain;
 
 	public String getHeader(String name) {
 		return request.getHeader(name);
@@ -55,6 +59,7 @@ public class ProductionRq implements Rq {
 	public void addCookie(String name, String value) {
 		Cookie accsessTokenCookie = new Cookie(name, value);
 
+		accsessTokenCookie.setDomain(domain);
 		accsessTokenCookie.setPath("/");
 		accsessTokenCookie.setHttpOnly(true);
 		accsessTokenCookie.setSecure(true);
@@ -66,6 +71,7 @@ public class ProductionRq implements Rq {
 	public void removeCookie(String name) {
 
 		Cookie cookie = new Cookie(name, null);
+		cookie.setDomain(domain);
 		cookie.setPath("/");
 		cookie.setHttpOnly(true);
 		cookie.setSecure(true);
