de24c27ce30c8c14d063a4e1a7f0a4781c2d0bf1
omegafrog <jiwoo22110103@gmail.com>
2025-03-18 11:40:57 +0900

    fix: 채팅창 저장 설정 추가

--- backend/src/main/java/com/NBE_4_5_2/Team5/global/config/WebSocketConfig.java
+++ backend/src/main/java/com/NBE_4_5_2/Team5/global/config/WebSocketConfig.java
@@ -1,7 +1,6 @@
 package com.NBE_4_5_2.Team5.global.config;
 
-import com.NBE_4_5_2.Team5.global.handler.StompHandler;
-import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
@@ -9,31 +8,37 @@ import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBr
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
+import com.NBE_4_5_2.Team5.global.handler.StompHandler;
+
+import lombok.RequiredArgsConstructor;
+
 @RequiredArgsConstructor
 @Configuration
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
-    private final StompHandler stompHandler;
+	private final StompHandler stompHandler;
 
-    @Override
-    public void configureMessageBroker(MessageBrokerRegistry config) {
-        config.enableSimpleBroker("/sub");
-        config.setApplicationDestinationPrefixes("/pub");
-    }
+	@Value("${custom.front.host}")
+	private String frontHost;
 
-    @Override
-    public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws-stomp")
-                .addInterceptors(new HttpHandshakeInterceptor())
-                .setAllowedOrigins("http://localhost:3000")
-                .withSockJS(); // sock.js를 통하여 낮은 버전의 브라우저에서도 websocket이 동작할수 있게 합니다.
-    }
+	@Override
+	public void configureMessageBroker(MessageBrokerRegistry config) {
+		config.enableSimpleBroker("/sub");
+		config.setApplicationDestinationPrefixes("/pub");
+	}
 
-    @Override
-    public void configureClientInboundChannel(ChannelRegistration registration) {
-        registration.interceptors(stompHandler);
-    }
+	@Override
+	public void registerStompEndpoints(StompEndpointRegistry registry) {
+		registry.addEndpoint("/ws-stomp")
+			.addInterceptors(new HttpHandshakeInterceptor())
+			.setAllowedOrigins("http://localhost:3000", "https://%s".formatted(frontHost))
+			.withSockJS(); // sock.js를 통하여 낮은 버전의 브라우저에서도 websocket이 동작할수 있게 합니다.
+	}
 
+	@Override
+	public void configureClientInboundChannel(ChannelRegistration registration) {
+		registration.interceptors(stompHandler);
+	}
 
 }
