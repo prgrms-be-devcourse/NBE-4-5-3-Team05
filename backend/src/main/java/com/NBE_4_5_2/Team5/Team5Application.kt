bf65e7eacde863d42561c61cf8b08a39bbb6ce4f
jeonghyeon-ohk <ok6737@naver.com>
2025-04-04 14:35:13 +0900

    refactor: Team5Application kt로 변환

--- backend/src/main/java/com/NBE_4_5_2/Team5/Team5Application.kt
+++ backend/src/main/java/com/NBE_4_5_2/Team5/Team5Application.kt
@@ -1,19 +1,19 @@
-package com.NBE_4_5_2.Team5;
+package com.NBE_4_5_2.Team5
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.ApplicationPidFileWriter;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.context.ApplicationPidFileWriter
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing
 
 @SpringBootApplication
 @EnableJpaAuditing
-public class Team5Application {
+class Team5Application {}
 
-	public static void main(String[] args) {
-		SpringApplication app = new SpringApplication(Team5Application.class);
-		// PID 파일을 기본 경로 (현재 디렉터리의 application.pid)로 저장
-		app.addListeners(new ApplicationPidFileWriter());
-		app.run(args);
-	}
+fun main(args: Array<String>) {
 
+    val app = SpringApplication(Team5Application::class.java)
+	// PID 파일을 기본 경로 (현재 디렉터리의 application.pid)로 저장
+	app.addListeners(ApplicationPidFileWriter())
+	app.run(*args)
 }
+
