ea18d35d5ee619c97a212d950f593b082a400e16
jeonghyeon-ohk <ok6737@naver.com>
2025-04-06 17:21:37 +0900

    refactor: UserRepository 코틀린 전환

--- backend/src/main/java/com/NBE_4_5_2/Team5/domain/user/user/repository/UserRepository.kt
+++ backend/src/main/java/com/NBE_4_5_2/Team5/domain/user/user/repository/UserRepository.kt
@@ -1,26 +1,14 @@
-package com.NBE_4_5_2.Team5.domain.user.user.repository;
+package com.NBE_4_5_2.Team5.domain.user.user.repository
 
-import java.util.List;
-import java.util.Optional;
+import com.NBE_4_5_2.Team5.domain.user.user.entity.Role
+import com.NBE_4_5_2.Team5.domain.user.user.entity.User
+import org.springframework.data.jpa.repository.JpaRepository
+import java.util.*
 
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.NBE_4_5_2.Team5.domain.user.user.entity.Role;
-import com.NBE_4_5_2.Team5.domain.user.user.entity.User;
-
-public interface UserRepository extends JpaRepository<User, String> {
-	Optional<User> findByUsername(String username);
-
-	Optional<User> findByEmail(String email);
-
-	Optional<User> findByNickname(String nickname);
-
-	List<User> findAllByRole(Role role);
-
-	// 이메일 중복 체크
-	boolean existsByEmail(String email);
-
-	boolean existsByUsername(String username);
-
-	boolean existsByNickname(String nickname);
+interface UserRepository : JpaRepository<User, String> {
+    fun findByUsername(username: String): Optional<User>
+    fun findAllByRole(role: Role): List<User?>
+    fun existsByEmail(email: String): Boolean // 이메일 중복 체크
+    fun existsByUsername(username: String): Boolean
+    fun existsByNickname(nickname: String): Boolean
 }
