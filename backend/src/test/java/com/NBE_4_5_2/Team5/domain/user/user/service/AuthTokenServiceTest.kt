11801add47d46a5b1c590f566669c0c36403c8cd
jeonghyeon-ohk <ok6737@naver.com>
2025-04-07 01:36:26 +0900

    test: AuthTokenServiceTest 코틀린 전환

--- backend/src/test/java/com/NBE_4_5_2/Team5/domain/user/user/service/AuthTokenServiceTest.kt
+++ backend/src/test/java/com/NBE_4_5_2/Team5/domain/user/user/service/AuthTokenServiceTest.kt
@@ -1,61 +1,56 @@
-package com.NBE_4_5_2.Team5.domain.user.user.service;
+package com.NBE_4_5_2.Team5.domain.user.user.service
 
-import static org.assertj.core.api.Assertions.*;
-
-import java.util.Map;
-
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.test.context.SpringBootTest;
-
-import com.NBE_4_5_2.Team5.domain.user.user.entity.User;
-import com.NBE_4_5_2.Team5.global.config.BaseTestConfig;
-import com.NBE_4_5_2.Team5.global.standard.util.Ut;
+import com.NBE_4_5_2.Team5.global.config.BaseTestConfig
+import com.NBE_4_5_2.Team5.global.standard.util.Ut
+import org.assertj.core.api.Assertions
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.boot.test.context.SpringBootTest
 
 @SpringBootTest
 @BaseTestConfig
-public class AuthTokenServiceTest  {
+class AuthTokenServiceTest {
 
-	@Autowired
-	private AuthTokenService authTokenService;
+    @Autowired
+    private lateinit var authTokenService: AuthTokenService
 
-	@Autowired
-	private UserService userService;
+    @Autowired
+    private lateinit var userService: UserService
 
-	@Value("${custom.jwt.secret-key}")
-	private String keyString;
+    @Value("\${custom.jwt.secret-key}")
+    private lateinit var keyString: String
 
-	@Test
-	@DisplayName("jwt : accessToken : user1 accessToken 생성 성공")
-	void accessToken() {
-		// Given
-		User user = userService.getUserByUsername("user1").orElseThrow();
+    @Test
+    @DisplayName("jwt : accessToken : user1 accessToken 생성 성공")
+    fun accessToken() {
+        // Given
+        val user = userService.getUserByUsername("user1").orElseThrow()
 
-		// When
-		String accessToken = authTokenService.generateAccessToken(user);
+        // When
+        val accessToken = authTokenService.generateAccessToken(user)
 
-		// Then
-		assertThat(accessToken).isNotBlank();
-		System.out.println("accessToken = " + accessToken);
-	}
+        // Then
+        Assertions.assertThat(accessToken).isNotBlank()
+        println("accessToken = $accessToken")
+    }
 
-	@Test
-	@DisplayName("jwt : 토큰 유효성 체크")
-	void checkValid() {
-		// Given
-		User user = userService.getUserByUsername("user1").orElseThrow();
-		String accessToken = authTokenService.generateAccessToken(user);
+    @Test
+    @DisplayName("jwt : 토큰 유효성 체크")
+    fun checkValid() {
+        // Given
+        val user = userService.getUserByUsername("user1").orElseThrow()
+        val accessToken = authTokenService.generateAccessToken(user)
 
-		// When
-		boolean isValid = Ut.Jwt.isValidToken(keyString, accessToken);
-		Map<String, Object> parsedPayload = authTokenService.getPayload(accessToken);
+        // When
+        val isValid = Ut.Jwt.isValidToken(keyString, accessToken)
+        val parsedPayload = authTokenService.getPayload(accessToken)
 
-		// Then
-		assertThat(isValid).isTrue();
-		assertThat(parsedPayload).containsAllEntriesOf(
-			Map.of("id", user.getId(), "username", user.getUsername())
-		);
-	}
+        // Then
+        Assertions.assertThat(isValid).isTrue()
+        Assertions.assertThat(parsedPayload).containsAllEntriesOf(
+            mapOf("id" to user.id, "username" to user.username)
+        )
+    }
 }
