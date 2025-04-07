167d4d1f8e2f2ab841b728fbefb7f7e1978293c5
omegafrog <jiwoo22110103@gmail.com>
2025-04-07 16:10:46 +0900

    feat: banlistBatchSchedular 변환

--- backend/src/main/java/com/NBE_4_5_2/Team5/batch/BanListBatchScheduler.kt
+++ backend/src/main/java/com/NBE_4_5_2/Team5/batch/BanListBatchScheduler.kt
@@ -1,27 +1,23 @@
-package com.NBE_4_5_2.Team5.batch;
+package com.NBE_4_5_2.Team5.batch
 
-import java.time.LocalDate;
-import java.util.List;
-
-import org.springframework.scheduling.annotation.Scheduled;
-import org.springframework.stereotype.Component;
-import org.springframework.transaction.annotation.Transactional;
-
-import com.NBE_4_5_2.Team5.domain.user.admin.entity.BanList;
-import com.NBE_4_5_2.Team5.domain.user.admin.repository.BanListRepository;
-
-import lombok.RequiredArgsConstructor;
+import com.NBE_4_5_2.Team5.domain.user.admin.entity.BanList
+import com.NBE_4_5_2.Team5.domain.user.admin.repository.BanListRepository
+import lombok.RequiredArgsConstructor
+import org.springframework.scheduling.annotation.Scheduled
+import org.springframework.stereotype.Component
+import org.springframework.transaction.annotation.Transactional
+import java.time.LocalDate
+import java.util.function.Consumer
 
 @Component
 @RequiredArgsConstructor
-public class BanListBatchScheduler {
+class BanListBatchScheduler {
+    private val banListRepository: BanListRepository? = null
 
-	private final BanListRepository banListRepository;
-
-	@Scheduled(cron = "0 0 0 * * *")
-	@Transactional
-	public void schedule() {
-		List<BanList> allByEndDateAfter = banListRepository.findAllBy_endDateIsAfter(LocalDate.now().atStartOfDay());
-		allByEndDateAfter.forEach(item -> item.getBannedUser().unBan());
-	}
+    @Scheduled(cron = "0 0 0 * * *")
+    @Transactional
+    fun schedule() {
+        val allByEndDateAfter = banListRepository!!.findAllBy_endDateIsAfter(LocalDate.now().atStartOfDay())
+        allByEndDateAfter.forEach(Consumer { item: BanList -> item.bannedUser.unBan() })
+    }
 }
