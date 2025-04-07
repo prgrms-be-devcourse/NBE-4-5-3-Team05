99c9cad949f7d2f37d3297ffe652a800b8aaaf18
omegafrog <jiwoo22110103@gmail.com>
2025-04-07 15:49:43 +0900

    feat: commentcontrollerTest 변환

--- backend/src/test/java/com/NBE_4_5_2/Team5/domain/user/admin/controller/AdminControllerTest.kt
+++ backend/src/test/java/com/NBE_4_5_2/Team5/domain/user/admin/controller/AdminControllerTest.kt
@@ -1,186 +1,191 @@
-package com.NBE_4_5_2.Team5.domain.user.admin.controller;
+package com.NBE_4_5_2.Team5.domain.user.admin.controller
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Optional;
-
-import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.Order;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestInstance;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.MediaType;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.ResultActions;
-
-import com.NBE_4_5_2.Team5.domain.post.post.service.ProductPostService;
-import com.NBE_4_5_2.Team5.domain.user.admin.dto.NoticeResBody;
-import com.NBE_4_5_2.Team5.domain.user.admin.entity.BanList;
-import com.NBE_4_5_2.Team5.domain.user.admin.repository.BanListRepository;
-import com.NBE_4_5_2.Team5.domain.user.admin.service.AdminService;
-import com.NBE_4_5_2.Team5.domain.user.user.entity.User;
-import com.NBE_4_5_2.Team5.domain.user.user.service.UserService;
-import com.NBE_4_5_2.Team5.global.config.BaseTestConfig;
-import com.NBE_4_5_2.Team5.global.exception.post.product.ProductPostNotFoundException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-
-import jakarta.servlet.http.Cookie;
+import com.NBE_4_5_2.Team5.domain.post.post.service.ProductPostService
+import com.NBE_4_5_2.Team5.domain.user.admin.repository.BanListRepository
+import com.NBE_4_5_2.Team5.domain.user.admin.service.AdminService
+import com.NBE_4_5_2.Team5.domain.user.user.service.UserService
+import com.NBE_4_5_2.Team5.global.config.BaseTestConfig
+import com.NBE_4_5_2.Team5.global.exception.post.product.ProductPostNotFoundException
+import com.fasterxml.jackson.databind.ObjectMapper
+import jakarta.servlet.http.Cookie
+import org.assertj.core.api.Assertions
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.TestInstance
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.http.MediaType
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers
+import java.util.*
 
 @SpringBootTest
 @AutoConfigureMockMvc
 @BaseTestConfig
-@Order(100)
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
-class AdminControllerTest {
+internal class AdminControllerTest(
+    @Autowired
+    private val mockMvc: MockMvc,
 
-	@Autowired
-	private MockMvc mockMvc;
-	@Autowired
-	private BanListRepository banListRepository;
-	@Autowired
-	private ObjectMapper objectMapper;
-	@Autowired
-	private UserService userService;
-	@Autowired
-	private ProductPostService productPostService;
-	@Autowired
-	private AdminService adminService;
+    @Autowired
+    private val banListRepository: BanListRepository,
 
-	@Test
-	void writeNotice() throws Exception {
-		//given
-		// 관리자로 로그인
-		Map<String, Cookie> cookieMap = login("user4", "user41234@");
+    @Autowired
+    private val objectMapper: ObjectMapper,
 
-		// when
-		// API 호출
-		ResultActions perform = mockMvc.perform(post("/api/admin/notices")
-			.content("""
+    @Autowired
+    private val userService: UserService,
+
+    @Autowired
+    private val productPostService: ProductPostService,
+
+    @Autowired
+    private val adminService: AdminService,
+) {
+
+    @Test
+    @Throws(Exception::class)
+    fun writeNotice() {
+        //given
+        // 관리자로 로그인
+        val cookieMap = login("user4", "user41234@")
+
+        // when
+        // API 호출
+        val perform = mockMvc!!.perform(
+            MockMvcRequestBuilders.post("/api/admin/notices")
+                .content(
+                    """
 				{
 					"title": "공지 제목",
 					"content": "공지 내용"
-				}""")
-			.contentType("application/json")
-			.characterEncoding("utf-8")
-			.cookie(cookieMap.get("accessToken"), cookieMap.get("refreshToken")));
+				}
+				""".trimIndent()
+                )
+                .contentType("application/json")
+                .characterEncoding("utf-8")
+                .cookie(cookieMap["accessToken"], cookieMap["refreshToken"])
+        )
 
-		String id = objectMapper.readTree(perform.andReturn().getResponse().getContentAsString())
-			.get("data").get("id").asText();
+        val id = objectMapper!!.readTree(perform.andReturn().response.contentAsString)["data"]["id"].asText()
 
-		NoticeResBody notice = adminService.getNotice(id);
+        val notice = adminService!!.getNotice(id)
 
-		User admin = userService.getUserByUsername("user4")
-			.orElseThrow(() -> new RuntimeException());
+        val admin = userService!!.getUserByUsername("user4")
+            .orElseThrow { RuntimeException() }
 
-		// then
-		perform
-			.andExpect(status().isOk())
-			.andExpect(handler().handlerType(AdminController.class))
-			.andExpect(handler().methodName("writeNotice"))
-			.andExpect(jsonPath("$.code").value("200-1"))
-			.andExpect(jsonPath("$.message").value("공지사항 등록 성공."))
-			.andExpect(jsonPath("$.data.id").value(notice.id))
-			.andExpect(jsonPath("$.data.title").value(notice.title))
-			.andExpect(jsonPath("$.data.content").value(notice.content))
-			.andExpect(jsonPath("$.data.admin.id").value(admin.getId()));
-	}
+        // then
+        perform
+            .andExpect(MockMvcResultMatchers.status().isOk())
+            .andExpect(MockMvcResultMatchers.handler().handlerType(AdminController::class.java))
+            .andExpect(MockMvcResultMatchers.handler().methodName("writeNotice"))
+            .andExpect(MockMvcResultMatchers.jsonPath("$.code").value("200-1"))
+            .andExpect(MockMvcResultMatchers.jsonPath("$.message").value("공지사항 등록 성공."))
+            .andExpect(MockMvcResultMatchers.jsonPath("$.data.id").value(notice.id))
+            .andExpect(MockMvcResultMatchers.jsonPath("$.data.title").value(notice.title))
+            .andExpect(MockMvcResultMatchers.jsonPath("$.data.content").value(notice.content))
+            .andExpect(MockMvcResultMatchers.jsonPath("$.data.admin.id").value(admin.id))
+    }
 
-	private Map<String, Cookie> login(String username, String password) throws Exception {
-		Cookie[] cookies = mockMvc.perform(post("/api/users/login")
-				.contentType(MediaType.APPLICATION_JSON)
-				.content("""
+    @Throws(Exception::class)
+    private fun login(username: String, password: String): Map<String, Cookie> {
+        val cookies = mockMvc!!.perform(
+            MockMvcRequestBuilders.post("/api/users/login")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(
+                    """
 					{
 						"username": "%s",
 						"password": "%s"
-					}""".formatted(username, password)))
-			.andExpect(status().isOk())
-			.andReturn().getResponse().getCookies();
+					}
+					""".trimIndent().formatted(username, password)
+                )
+        )
+            .andExpect(MockMvcResultMatchers.status().isOk())
+            .andReturn().response.cookies
 
-		Map<String, Cookie> cookieMap = new HashMap<>();
+        val cookieMap: MutableMap<String, Cookie> = HashMap()
 
-		Arrays.stream(cookies)
-			.filter(cookie -> cookie.getName().equals("accessToken") || cookie.getName().equals("refreshToken"))
-			.forEach(cookie -> cookieMap.put(cookie.getName(), cookie));
-		return cookieMap;
-	}
+        Arrays.stream(cookies)
+            .filter { cookie: Cookie -> cookie.name == "accessToken" || cookie.name == "refreshToken" }
+            .forEach { cookie: Cookie -> cookieMap[cookie.name] = cookie }
+        return cookieMap
+    }
 
-	@Test
-	void banUser() throws Exception {
-		//given
-		// 관리자 로그인
-		Map<String, Cookie> cookieMap = login("user4", "user41234@");
+    @Test
+    @Throws(Exception::class)
+    fun banUser() {
+        //given
+        // 관리자 로그인
+        val cookieMap = login("user4", "user41234@")
 
-		// when
-		// user1을 밴함
+        // when
+        // user1을 밴함
+        val user = userService!!.getUserByUsername("user1")
+            .orElseThrow { RuntimeException() }
 
-		User user = userService.getUserByUsername("user1")
-			.orElseThrow(() -> new RuntimeException());
-
-		ResultActions perform = mockMvc.perform(post("/api/admin/users/%s/ban".formatted(user.getId()))
-			.content("""
+        val perform = mockMvc!!.perform(
+            MockMvcRequestBuilders.post("/api/admin/users/%s/ban".formatted(user.id))
+                .content(
+                    """
 				{
 					"reason": "기분이 나빠서"
-				}""")
-			.contentType("application/json")
-			.characterEncoding("utf-8")
-			.cookie(cookieMap.get("accessToken"), cookieMap.get("refreshToken")));
+				}
+				""".trimIndent()
+                )
+                .contentType("application/json")
+                .characterEncoding("utf-8")
+                .cookie(cookieMap["accessToken"], cookieMap["refreshToken"])
+        )
 
-		String id = objectMapper.readTree(perform.andReturn().getResponse().getContentAsString())
-			.get("data").get("banListId").asText();
+        val id = objectMapper!!.readTree(perform.andReturn().response.contentAsString)["data"]["banListId"].asText()
 
-		BanList banList = banListRepository.findById(id).get();
+        val banList = banListRepository!!.findById(id).get()
 
-		// then
+        // then
+        val foundedUser = userService.getUserByUsername("user1")
 
-		Optional<User> foundedUser = userService.getUserByUsername("user1");
+        perform
+            .andExpect(MockMvcResultMatchers.status().isOk())
+            .andExpect(MockMvcResultMatchers.handler().handlerType(AdminController::class.java))
+            .andExpect(MockMvcResultMatchers.handler().methodName("banUser"))
+            .andExpect(MockMvcResultMatchers.jsonPath("$.code").value("200-1"))
+            .andExpect(MockMvcResultMatchers.jsonPath("$.message").value("유저 정지 성공"))
+            .andExpect(MockMvcResultMatchers.jsonPath("$.data.banListId").value(banList.id))
+            .andExpect(MockMvcResultMatchers.jsonPath("$.data.reason").value(banList.reason))
+            .andExpect(MockMvcResultMatchers.jsonPath("$.data.userId").value(banList.bannedUser.id))
+            .andExpect(MockMvcResultMatchers.jsonPath("$.data.banCount").value(foundedUser.get().blockedCount))
+        Assertions.assertThat(foundedUser.get().blocked).isTrue()
+        Assertions.assertThat(foundedUser.get().blockedCount).isEqualTo(1)
+    }
 
-		perform
-			.andExpect(status().isOk())
-			.andExpect(handler().handlerType(AdminController.class))
-			.andExpect(handler().methodName("banUser"))
-			.andExpect(jsonPath("$.code").value("200-1"))
-			.andExpect(jsonPath("$.message").value("유저 정지 성공"))
-			.andExpect(jsonPath("$.data.banListId").value(banList.getId()))
-			.andExpect(jsonPath("$.data.reason").value(banList.getReason()))
-			.andExpect(jsonPath("$.data.userId").value(banList.getBannedUser().getId()))
-			.andExpect(jsonPath("$.data.banCount").value(foundedUser.get().getBlockedCount()));
-		Assertions.assertThat(foundedUser.get().getBlocked()).isTrue();
-		Assertions.assertThat(foundedUser.get().getBlockedCount()).isEqualTo(1);
-	}
+    @Test
+    @Throws(Exception::class)
+    fun deletePost() {
+        //given
+        // 로그인
+        val cookieMap = login("user4", "user41234@")
 
-	@Test
-	void deletePost() throws Exception {
-		//given
-		// 로그인
-		Map<String, Cookie> cookieMap = login("user4", "user41234@");
+        //when
+        // id를 가진 post를 삭제
+        val id = productPostService!!.getPosts(1, 1, "", "asc")
+            .items[0].id
 
-		//when
-		// id를 가진 post를 삭제
+        val result = mockMvc!!.perform(
+            MockMvcRequestBuilders.delete("/api/admin/posts/%s".formatted(id))
+                .cookie(cookieMap["accessToken"], cookieMap["refreshToken"])
+        )
 
-		String id = productPostService.getPosts(1, 1, "", "asc")
-			.getItems().get(0).getId();
+        // then
 
-		ResultActions result = mockMvc.perform(delete("/api/admin/posts/%s".formatted(id))
-			.cookie(cookieMap.get("accessToken"), cookieMap.get("refreshToken")));
+        // 삭제된 product post 조회 시 오류 발생해야 한다.
+        Assertions.assertThatThrownBy { productPostService.getPost(id) }
+            .isInstanceOf(ProductPostNotFoundException::class.java)
 
-		// then
-
-		// 삭제된 product post 조회 시 오류 발생해야 한다.
-
-		Assertions.assertThatThrownBy(
-			() -> productPostService.getPost(id)
-		).isInstanceOf(ProductPostNotFoundException.class);
-
-		result
-			.andExpect(status().isOk())
-			.andExpect(jsonPath("$.code").value("204-1"))
-			.andExpect(jsonPath("$.message").value("게시글 삭제 성공."));
-
-	}
+        result
+            .andExpect(MockMvcResultMatchers.status().isOk())
+            .andExpect(MockMvcResultMatchers.jsonPath("$.code").value("204-1"))
+            .andExpect(MockMvcResultMatchers.jsonPath("$.message").value("게시글 삭제 성공."))
+    }
 }
