bb26a87dee46e4d344488e9ae3fb43b16499bea2
omegafrog <jiwoo22110103@gmail.com>
2025-04-07 14:45:48 +0900

    feat: noticeResBody 변환

--- backend/src/main/java/com/NBE_4_5_2/Team5/domain/user/admin/dto/NoticeResBody.kt
+++ backend/src/main/java/com/NBE_4_5_2/Team5/domain/user/admin/dto/NoticeResBody.kt
@@ -1,13 +1,23 @@
-package com.NBE_4_5_2.Team5.domain.user.admin.dto;
+package com.NBE_4_5_2.Team5.domain.user.admin.dto
 
-import java.time.LocalDateTime;
+import com.NBE_4_5_2.Team5.domain.user.admin.entity.NoticePost
+import java.time.LocalDateTime
 
-import com.NBE_4_5_2.Team5.domain.user.admin.entity.NoticePost;
-
-public record NoticeResBody(String id, String title, String content, AdminResBody admin, LocalDateTime createdAt) {
-	public static NoticeResBody of(NoticePost saved) {
-		return new NoticeResBody(saved.getId(), saved.getTitle(), saved.getContent(),
-			new AdminResBody(saved.getAdmin()), saved.createdDate);
-	}
-
+@JvmRecord
+data class NoticeResBody(
+	@JvmField val id: String,
+	@JvmField val title: String,
+	@JvmField val content: String,
+	val admin: AdminResBody,
+	val createdAt: LocalDateTime
+) {
+    companion object {
+        @JvmStatic
+		fun of(saved: NoticePost): NoticeResBody {
+            return NoticeResBody(
+                saved.id, saved.title, saved.content,
+                AdminResBody(saved.admin), saved.createdDate
+            )
+        }
+    }
 }
