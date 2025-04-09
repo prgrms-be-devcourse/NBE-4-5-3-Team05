097535d2442e709271083a6acf94da217565d2d2
jeonghyeon-ohk <ok6737@naver.com>
2025-04-09 14:05:13 +0900

    refactor: ProductMetadataRepository 코틀린 전환

--- backend/src/main/java/com/NBE_4_5_2/Team5/domain/post/post/repository/ProductMetadataRepository.kt
+++ backend/src/main/java/com/NBE_4_5_2/Team5/domain/post/post/repository/ProductMetadataRepository.kt
@@ -1,11 +1,8 @@
-package com.NBE_4_5_2.Team5.domain.post.post.repository;
+package com.NBE_4_5_2.Team5.domain.post.post.repository
 
-import java.util.List;
+import com.NBE_4_5_2.Team5.domain.post.post.entity.ProductMetadata
+import org.springframework.data.jpa.repository.JpaRepository
 
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.NBE_4_5_2.Team5.domain.post.post.entity.ProductMetadata;
-
-public interface ProductMetadataRepository extends JpaRepository<ProductMetadata, String> {
-	List<ProductMetadata> findByName(String name);
+interface ProductMetadataRepository : JpaRepository<ProductMetadata, String> {
+    fun findByName(name: String): List<ProductMetadata>
 }
