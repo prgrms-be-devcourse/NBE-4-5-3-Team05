06ecf458b7091f26b42d02f04a245d78bca0f0bb
jeonghyeon-ohk <ok6737@naver.com>
2025-04-10 14:14:30 +0900

    test: 메인관리자 테스트 코드 추가

--- backend/src/test/java/com/NBE_4_5_2/Team5/domain/user/admin/controller/AdminControllerTest.kt
+++ backend/src/test/java/com/NBE_4_5_2/Team5/domain/user/admin/controller/AdminControllerTest.kt
@@ -188,4 +188,122 @@ internal class AdminControllerTest(
             .andExpect(MockMvcResultMatchers.jsonPath("$.code").value("204-1"))
             .andExpect(MockMvcResultMatchers.jsonPath("$.message").value("게시글 삭제 성공."))
     }
+
+    @Test
+    @Throws(Exception::class)
+    fun writeNoticeBySuperAdmin() {
+        //given
+        // 메인 관리자로 로그인
+        val cookieMap = login("admin1", "password1")
+
+        // when
+        // API 호출
+        val perform = mockMvc.perform(
+            MockMvcRequestBuilders.post("/api/admin/notices")
+                .content(
+                    """
+				{
+					"title": "공지 제목",
+					"content": "공지 내용"
+				}
+				""".trimIndent()
+                )
+                .contentType("application/json")
+                .characterEncoding("utf-8")
+                .cookie(cookieMap["accessToken"], cookieMap["refreshToken"])
+        )
+
+        val id = objectMapper.readTree(perform.andReturn().response.contentAsString)["data"]["id"].asText()
+
+        val notice = adminService.getNotice(id)
+
+        val admin = userService.getUserByUsername("admin1")
+            .orElseThrow { RuntimeException() }
+
+        // then
+        perform
+            .andExpect(MockMvcResultMatchers.status().isOk())
+            .andExpect(MockMvcResultMatchers.jsonPath("$.code").value("200-1"))
+            .andExpect(MockMvcResultMatchers.jsonPath("$.message").value("공지사항 등록 성공."))
+            .andExpect(MockMvcResultMatchers.jsonPath("$.data.id").value(notice.id))
+            .andExpect(MockMvcResultMatchers.jsonPath("$.data.title").value(notice.title))
+            .andExpect(MockMvcResultMatchers.jsonPath("$.data.content").value(notice.content))
+            .andExpect(MockMvcResultMatchers.jsonPath("$.data.admin.id").value(admin.id))
+    }
+
+    @Test
+    @Throws(Exception::class)
+    fun banUserBySuperAdmin() {
+        //given
+        // 메인 관리자로 로그인
+        val cookieMap = login("admin1", "password1")
+
+        // when
+        // user1을 밴함
+        val user = userService.getUserByUsername("user1")
+            .orElseThrow { RuntimeException() }
+
+        val perform = mockMvc.perform(
+            MockMvcRequestBuilders.post("/api/admin/users/${user.id}/ban")
+                .content(
+                    """
+				{
+					"reason": "기분이 나빠서"
+				}
+				""".trimIndent()
+                )
+                .contentType("application/json")
+                .characterEncoding("utf-8")
+                .cookie(cookieMap["accessToken"], cookieMap["refreshToken"])
+        )
+
+        val id = objectMapper.readTree(perform.andReturn().response.contentAsString)["data"]["banListId"].asText()
+
+        val banList = banListRepository.findById(id).get()
+
+        // then
+        val foundedUser = userService.getUserByUsername("user1")
+
+        perform
+            .andExpect(MockMvcResultMatchers.status().isOk())
+            .andExpect(MockMvcResultMatchers.handler().handlerType(AdminController::class.java))
+            .andExpect(MockMvcResultMatchers.handler().methodName("banUser"))
+            .andExpect(MockMvcResultMatchers.jsonPath("$.code").value("200-1"))
+            .andExpect(MockMvcResultMatchers.jsonPath("$.message").value("유저 정지 성공"))
+            .andExpect(MockMvcResultMatchers.jsonPath("$.data.banListId").value(banList.id))
+            .andExpect(MockMvcResultMatchers.jsonPath("$.data.reason").value(banList.reason))
+            .andExpect(MockMvcResultMatchers.jsonPath("$.data.userId").value(banList.bannedUser.id))
+            .andExpect(MockMvcResultMatchers.jsonPath("$.data.banCount").value(foundedUser.get().blockedCount))
+        Assertions.assertThat(foundedUser.get().blocked).isTrue()
+        Assertions.assertThat(foundedUser.get().blockedCount).isEqualTo(1)
+    }
+
+    @Test
+    @Throws(Exception::class)
+    fun deletePostBySuperAdmin() {
+        //given
+        // 메인 관리자로 로그인
+        val cookieMap = login("admin1", "password1")
+
+        //when
+        // id를 가진 post를 삭제
+        val id = productPostService.getPosts(1, 1, "", "asc", 0, 1000000, emptyList())
+            .items[0].id
+
+        val result = mockMvc.perform(
+            MockMvcRequestBuilders.delete("/api/admin/posts/$id")
+                .cookie(cookieMap["accessToken"], cookieMap["refreshToken"])
+        )
+
+        // then
+
+        // 삭제된 product post 조회 시 오류 발생해야 한다.
+        Assertions.assertThatThrownBy { productPostService.getPost(id) }
+            .isInstanceOf(ProductPostNotFoundException::class.java)
+
+        result
+            .andExpect(MockMvcResultMatchers.status().isNoContent)
+            .andExpect(MockMvcResultMatchers.jsonPath("$.code").value("204-1"))
+            .andExpect(MockMvcResultMatchers.jsonPath("$.message").value("게시글 삭제 성공."))
+    }
 }
