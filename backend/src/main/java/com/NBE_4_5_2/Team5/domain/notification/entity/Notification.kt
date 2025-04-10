d07aa9838caa2e3dd8a357d612e78c1e17ff2b88
Kim Ji Woo <jiwoo22110103@gmail.com>
2025-04-10 17:16:59 +0900

    fix: 알림 코틀린 전환 이후 안되는 오류 수정 (#56)
    * fix: 알림 코틀린 전환 이후 안되는 오류 수정

--- backend/src/main/java/com/NBE_4_5_2/Team5/domain/notification/entity/Notification.kt
+++ backend/src/main/java/com/NBE_4_5_2/Team5/domain/notification/entity/Notification.kt
@@ -1,25 +1,31 @@
 package com.NBE_4_5_2.Team5.domain.notification.entity
 
-import jakarta.persistence.Entity
-import jakarta.persistence.GeneratedValue
-import jakarta.persistence.GenerationType
-import jakarta.persistence.Id
+import com.fasterxml.jackson.annotation.JsonProperty
+import jakarta.persistence.*
 import java.util.logging.Logger.global
 
 @Entity
 class Notification(
+    @Column(name="user_id")
+    @JsonProperty(value = "userId")
     private var _userId: String?,
+    @Column(name = "global")
+    @JsonProperty(value="global")
     private var _isGlobal: Boolean = false,
+    @Column(name = "content")
+    @JsonProperty(value="content")
     private var _content: String?=null
 ) {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    @JsonProperty(value = "id")
     private var _id: Long? = null
     val id: Long
         get() = _id!!
 
-    val userId: String
-        get()=_userId!!
+    val userId: String?
+        get()=_userId
 
     val global: Boolean
         get()=_isGlobal
@@ -33,4 +39,11 @@ class Notification(
         _isGlobal = global,
         _content = content
     )
+    constructor(id:Long, userId:String?, global:Boolean, content:String?):this(
+        _userId=userId,
+        _isGlobal=global,
+        _content= content,
+    ){
+        _id=id
+    }
 }
